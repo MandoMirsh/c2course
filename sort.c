12f744d8c7e8d44c7cb0bb9cb605afb797cc7977
MandoMirsh <33557863+MandoMirsh@users.noreply.github.com>
2017-11-18 15:58:56 +0400

    Add files via upload

--- sort.c
+++ sort.c
@@ -1,3 +1,25 @@
+/*
+ * testa.c
+ * 
+ * Copyright 2017 Черевков С.Н. <s02160255@debian>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -462,7 +484,6 @@ void mfunc(char **sargs,int sargc,int fdout,int r)
 
 int main(int argc, char **argv)
 {
-	int fd1,fd2,fd3;
 	int i,flen;
 	char *s=NULL/*!!!*/,*s1=NULL/*!!!*/,*s2;
 	char **sargs;
